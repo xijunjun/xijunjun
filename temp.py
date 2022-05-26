c3c276cd303495c4c91ef14ca151b1d2fff3f218
taoshuai <1107291820@qq.com>
2022-05-27 00:16:07 +0800

    1

--- temp.py
+++ temp.py
@@ -1,113 +1,180 @@
-1#coding:utf-8
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-from PIL import ImageFont
-from PIL import Image
-from PIL import ImageDraw
-import cv2
+"""Generate images using pretrained network pickle."""
+
 import os
-import numpy as np
-import shutil
-import platform
+import re
+from typing import List, Optional
 
-import argparse
-import cv2
+import click
+import dnnlib
+import numpy as np
+import PIL.Image
 import torch
 
-from facexlib.alignment import init_alignment_model, landmark_98_to_68
-from facexlib.detection import init_detection_model
-from facexlib.visualization import visualize_alignment
+import legacy
+import dnnlib
+from torch_utils import misc
+
+#----------------------------------------------------------------------------
+
+def num_range(s: str) -> List[int]:
+    '''Accept either a comma separated list of numbers 'a,b,c' or a range 'a-c' and return as a list of ints.'''
+
+    range_re = re.compile(r'^(\d+)-(\d+)$')
+    m = range_re.match(s)
+    if m:
+        return list(range(int(m.group(1)), int(m.group(2))+1))
+    vals = s.split(',')
+    return [int(x) for x in vals]
+
+#----------------------------------------------------------------------------
+
+@click.command()
+@click.pass_context
+@click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
+@click.option('--seeds', type=num_range, help='List of random seeds')
+@click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
+@click.option('--class', 'class_idx', type=int, help='Class label (unconditional if not specified)')
+@click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
+@click.option('--projected-w', help='Projection result file', type=str, metavar='FILE')
+@click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
+def generate_images(
+    ctx: click.Context,
+    network_pkl: str,
+    seeds: Optional[List[int]],
+    truncation_psi: float,
+    noise_mode: str,
+    outdir: str,
+    class_idx: Optional[int],
+    projected_w: Optional[str]
+):
+    """Generate images using pretrained network pickle.
+
+    Examples:
+
+    \b
+    # Generate curated MetFaces images without truncation (Fig.10 left)
+    python generate.py --outdir=out --trunc=1 --seeds=85,265,297,849 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+
+    \b
+    # Generate uncurated MetFaces images with truncation (Fig.12 upper left)
+    python generate.py --outdir=out --trunc=0.7 --seeds=600-605 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+
+    \b
+    # Generate class conditional CIFAR-10 images (Fig.17 left, Car)
+    python generate.py --outdir=out --seeds=0-35 --class=1 \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl
+
+    \b
+    # Render an image from projected W
+    python generate.py --outdir=out --projected_w=projected_w.npz \\
+        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+    """
+
+    print('Loading networks from "%s"...' % network_pkl)
+    device = torch.device('cuda')
+    with dnnlib.util.open_url(network_pkl) as f:
+        Gpkl = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
+
+    kwargs = dnnlib.EasyDict(
+        z_dim=512,
+        c_dim=0,
+        w_dim=512,
+        img_resolution=1024,
+        img_channels=3,
+        mapping_kwargs=dnnlib.EasyDict(
+            num_layers=8,
+            embed_features= None,
+            layer_features= None,
+            activation= 'lrelu',
+            lr_multiplier= 0.01,
+            # w_avg_beta=kwarg('w_avg_beta', 0.995, none=1),
+        ),
+        synthesis_kwargs=dnnlib.EasyDict(
+            channel_base= 16384 * 2,
+            channel_max=512,
+            num_fp16_res=0,
+            conv_clamp= None,
+            architecture='skip',
+            resample_filter= [1, 3, 3, 1],
+            use_noise= True,
+            activation= 'lrelu',
+        ),
+    )
+
+    from training import networks
+    G = networks.Generator(**kwargs).eval().requires_grad_(False).to(device)
+
+
+
+
+    # G = dnnlib.util.construct_class_by_name(**G_kwargs, **common_kwargs).train().requires_grad_(False).to(device) # subclass of torch.nn.Module
+    # D = dnnlib.util.construct_class_by_name(**D_kwargs, **common_kwargs).train().requires_grad_(False).to(device) # subclass of torch.nn.Module
+
+
+
+    # misc.copy_params_and_buffers(Gpkl, G, require_all=False)
+
+    gkeys=G.state_dict()
+    pretrain_dict={k:v for k,v in Gpkl.named_parameters() if k in gkeys }
+    gkeys.update(pretrain_dict)
+    G.load_state_dict(gkeys)
 
 
 
 
 
-if __name__=='__main__':
-    cap = cv2.VideoCapture(0)
-    align_net = init_alignment_model('awing_fan')
-    det_net = init_detection_model('retinaface_resnet50', half=False)
+    os.makedirs(outdir, exist_ok=True)
 
-    face_size=144
-    face_template = np.array([[192.98138, 239.94708], [318.90277, 240.1936], [256.63416, 314.01935],
-                                   [201.26117, 371.41043], [313.08905, 371.15118]])
-    face_template = face_template * (face_size / 512.0)
+    # Synthesize the result of a W projection.
+    if projected_w is not None:
+        if seeds is not None:
+            print ('warn: --seeds is ignored when using --projected-w')
+        print(f'Generating images from projected W "{projected_w}"')
+        ws = np.load(projected_w)['w']
+        ws = torch.tensor(ws, device=device) # pylint: disable=not-callable
+        assert ws.shape[1:] == (G.num_ws, G.w_dim)
+        for idx, w in enumerate(ws):
+            img = G.synthesis(w.unsqueeze(0), noise_mode=noise_mode)
+            img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+            img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/proj{idx:02d}.png')
+        return
 
-    while 1:
-        with torch.no_grad():
-            ret, frame = cap.read()
-            frame=cv2.flip(frame, 1)
+    if seeds is None:
+        ctx.fail('--seeds option is required when not using --projected-w')
 
-            bboxes = det_net.detect_faces(frame, 0.97)
-            for box in bboxes:
-                # print (box)
-                rct=box[0:4].astype(np.int)
-                land=box[5:5+10].reshape((5,2)).astype(np.int)
+    # Labels.
+    label = torch.zeros([1, G.c_dim], device=device)
+    if G.c_dim != 0:
+        if class_idx is None:
+            ctx.fail('Must specify class label with --class when using a conditional network')
+        label[:, class_idx] = 1
+    else:
+        if class_idx is not None:
+            print ('warn: --class=lbl ignored when running on an unconditional network')
 
-                cv2.rectangle(frame, (rct[0], rct[1]), (rct[2], rct[3]), (0, 0, 255), 2)
-                for pt in land:
-                    print (pt)
-                    cv2.circle(frame,(pt[0],pt[1]),3,(255,0,0),-1,-1)
-
-                affine_matrix = cv2.estimateAffinePartial2D(land, face_template, method=cv2.LMEDS)[0]
-                cropped_face = cv2.warpAffine(frame, affine_matrix, (144,144), borderMode=0,borderValue=(135, 133, 132))  # gray
-                cv2.imshow("cropped_face", cropped_face)
-
-            landmarks = align_net.get_landmarks(frame)
-            landmarks=landmarks.astype(np.int)
-
-
-
-            # print (landmarks)
-            #
-            # for pt in landmarks:
-            #     cv2.circle(frame,(pt[0],pt[1]),3,(255,0,0),-1,-1)
+    # Generate images.
+    for seed_idx, seed in enumerate(seeds):
+        print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
+        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+        img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+        img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
 
 
-            cv2.imshow("capture", frame)
-            if cv2.waitKey(10) & 0xff == ord('q'):
-                break
-    cap.release()
-    cv2.destroyAllWindows()
+#----------------------------------------------------------------------------
 
+if __name__ == "__main__":
+    generate_images() # pylint: disable=no-value-for-parameter
 
+#----------------------------------------------------------------------------
 
-
-    # align_net = init_alignment_model('awing_fan')
-    #
-    # imtxt='/disks/disk0/Dataset/FFHQ/image1024x1024/images1024x1024/ffhq_1024_imlist.txt'
-    # dsttxt='/disks/disk0/Dataset/FFHQ/image1024x1024/images1024x1024/ffhq_1024_landmark.txt'
-    #
-    # with open(imtxt,'r') as f:
-    #     lines=f.readlines()
-    #
-    # allano=''
-    #
-    #
-    #
-    #
-    #
-    # for i,line in enumerate(lines):
-    #     impath=line.rstrip('\n')
-    #     img=cv2.imread(impath)
-    #     landmarks = align_net.get_landmarks(img)
-    #     landmarks=landmarks.astype(np.int)
-    #
-    #     # print (landmarks)
-    #
-    #     oneano=impath+' 98'
-    #     for pt in landmarks:
-    #         # print (pt)
-    #         # cv2.circle(img,(pt[0],pt[1]),3,(255,0,0),-1,-1)
-    #         oneano+=' '+str(pt[0])+' '+str(pt[1])
-    #     allano+=oneano+'\n'
-    #
-    #
-    #     # cv2.imshow('img',img)
-    #     # key=cv2.waitKey(0)
-    #     # if key==27:
-    #     #     exit(0)
-    #     # if i==10:
-    #     #     break
-    #     print (i)
-    # allano=allano.rstrip('\n')
-    # with open(dsttxt,'w') as f:
-    #     f.writelines(allano)
